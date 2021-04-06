362cbd635ad92fb9ea02ae3dabe82307b76ca771
Stanislav <te100ff@inbox.ru>
2021-04-06 10:07:10 +0300

    all tasks except logic

--- PersonalQuiz/ViewControllers/ResultsViewController.swift
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -14,12 +14,38 @@ class ResultsViewController: UIViewController {
     // 3. Отобразить результат в соответсвии с этим животным
     // 4. Избавиться от кнопки возврата назад на экране результатов
     
+    
+    @IBOutlet weak var showResultLabel: UILabel!
+    @IBOutlet weak var descritionLabel: UILabel!
+    
+    
     var answersChoosen: [Answer]!
+   // private var
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        navigationItem.hidesBackButton = true
+    }
+    
+    private func defineUserType() -> AnimalType {
+        for answer in answersChoosen {
+            switch answer.type {
+            
+            case .dog:
+                <#code#>
+            case .cat:
+                <#code#>
+            case .rabbit:
+                <#code#>
+            case .turtle:
+                <#code#>
+            }
+        }
+    }
+    
+    private func showResult(type: AnimalType) {
+        showResultLabel.text = "Вы - \(type.rawValue)"
+        descritionLabel.text = type.definition
     }
 
 }
